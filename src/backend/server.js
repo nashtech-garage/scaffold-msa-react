048fce139029a068f933450ca85c5c822e6d41e4
Le Cao Thi Hoang <lecao@nashtechglobal.com>
2024-10-03 22:52:19 +0700

    Create msa Reactjs app

--- src/backend/server.js
+++ src/backend/server.js
@@ -19,7 +19,9 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+//app.use(express.static(path.join(__dirname, "public")));
+app.use('/${{ values.frontendName }}', express.static(path.join(__dirname, 'public')));
+
 
 //  adding routes
 require("./routes")(app);
